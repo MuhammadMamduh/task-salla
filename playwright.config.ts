3de30222a6307f1709d0dde12a6a8b6b1d0d2442
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2024-09-04 17:14:20 +0300

    [task-salla] Update - configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html"], ["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -44,7 +44,7 @@ export default defineConfig({
     launchOptions: {
       // 1
       // args: ["--start-maximized"],
-      // slowMo: 3_00,
+      slowMo: 2_00,
     },
   },
 
@@ -90,4 +90,5 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+}
+);
